9600b2841a74c0df0b73c178ee0e098770cf343a
mateen <mateenworld7861@gmail.com>
2020-10-29 17:35:37 +0500

    fewfa

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
-
-
 const NotFound = () => {
-    return (<div>
-
-        <h1>Page not found</h1>
-        <p>
-            <Link to="/">Head home</Link>
-        </p>
+  return (
+    <div>
+      <h1>Your lolly is freezing</h1>
+      <h1>Retry after 2-3 minutes</h1>
     </div>
-    )
+  )
 }
 
 export default NotFound
