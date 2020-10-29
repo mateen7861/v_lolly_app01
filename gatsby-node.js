42ed583ff295ffb95e23af583c2989edf66fe004
mateen <mateenworld7861@gmail.com>
2020-10-29 17:32:13 +0500

    fewfa

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const path = require("path")
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
+
   const { data } = await graphql(`
     {
       get_lollies {
